13ca6c95acfb94c089d26b9df97293cd5b195dd7
Datrinon <dtrinh089@gmail.com>
2021-04-28 13:45:06 -0700

    Start Project: Created MainActivity, small start on HistoryActivity
    Created the MainActivity and created the files for the History screen.

---
+++ app/src/main/java/com/trinhdan/easywriter/MainActivity.java
@@ -0,0 +1,43 @@
+package com.trinhdan.easywriter;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    private Button startGame;
+    private Button viewHistory;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get buttons and register click listeners to them.
+        startGame = findViewById(R.id.main_StartGame);
+        viewHistory = findViewById(R.id.main_ViewHistory);
+
+        // Start game should... start a new game.
+        startGame.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+
+        // View history should... take you to an activity that allows one to view the previous freewrites.
+        viewHistory.setOnClickListener(new View.OnClickListener(){
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(this, ViewHistoryActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
