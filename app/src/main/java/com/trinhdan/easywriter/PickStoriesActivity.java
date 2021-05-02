8c4780eaae77bef8ce31d629d3e2b98f4f9382df
Datrinon <dtrinh089@gmail.com>
2021-05-02 14:18:36 -0700

    Imported Dice Pictures and Added Structure to collect IDs fro mthem

--- app/src/main/java/com/trinhdan/easywriter/PickStoriesActivity.java
+++ app/src/main/java/com/trinhdan/easywriter/PickStoriesActivity.java
@@ -2,8 +2,11 @@ package com.trinhdan.easywriter;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.Resources;
 import android.os.Bundle;
 import android.util.Log;
+
+import java.util.ArrayList;
 
 public class PickStoriesActivity extends AppCompatActivity {
 
@@ -13,5 +16,16 @@ public class PickStoriesActivity extends AppCompatActivity {
         setContentView(R.layout.activity_pick_stories);
 
         Log.e("Test", "Game Manager Says: " + FreeWriteConfigManager.getInstance().getDiceQuantity());
+
+        ArrayList<Integer> picArr = new ArrayList<>();
+
+
+        for(int i = 1; i <= 116; i++){
+            picArr.add(getResources().getIdentifier("pic_"+i,
+                                            "drawable", "com.trinhdan.easywriter"));
+        }
+
+        Log.d("Pause", "Dab");
+
     }
 }
